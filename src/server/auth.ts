0a44ac7cc98bda64b07a934edda90919b2f697d0
Iqbal Pamula <balpamula@gmail.com>
2025-04-28 19:55:41 +0700

    Initial Commit🎉

---
+++ src/server/auth.ts
@@ -0,0 +1,89 @@
+import {
+  getServerSession,
+  type DefaultSession,
+  type NextAuthOptions,
+} from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/server/db";
+import { env } from "@/env";
+import type { Adapter } from "next-auth/adapters";
+
+/**
+ * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+ * object and keep type safety.
+ *
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ */
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: DefaultSession["user"] & User;
+  }
+
+  interface User {
+    id: string;
+    firstName: string;
+    lastName?: string;
+    username: string;
+  }
+}
+
+/**
+ * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
+ *
+ * @see https://next-auth.js.org/configuration/options
+ */
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db) as Adapter,
+  session: {
+    strategy: "jwt",
+  },
+  secret: env.NEXTAUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      type: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user, trigger, session }) {
+      if (user) {
+        token = { ...token, ...user };
+      }
+
+      if (trigger === "update" && (session as { username: string })?.username) {
+        token.username = (session as { username: string }).username;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      session = {
+        ...session,
+        user: {
+          ...session.user,
+          ...token,
+        },
+      };
+
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+};
+
+/**
+ * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
+ *
+ * @see https://next-auth.js.org/configuration/nextjs
+ */
+export const getServerAuthSession = () => getServerSession(authOptions);
